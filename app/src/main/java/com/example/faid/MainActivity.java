d482c8163ba33efb3050a8195550ffcb5b161d61
Laxya Pahuja <laxya.pahuja8@gmail.com>
2019-04-24 01:24:33 +0530

    ui

--- app/src/main/java/com/example/faid/MainActivity.java
+++ app/src/main/java/com/example/faid/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.faid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView=findViewById(R.id.signup);
+        textView.bringToFront();
     }
 }
