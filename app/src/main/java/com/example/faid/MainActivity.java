fb2f22d72c7ecda44f933f44b166e6da09c0d21a
ribhav <ribhavsharma2003@gmail.com>
2019-04-25 00:37:52 +0530

    comit

--- app/src/main/java/com/example/faid/MainActivity.java
+++ app/src/main/java/com/example/faid/MainActivity.java
@@ -1,17 +1,25 @@
 package com.example.faid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.WindowManager;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView textView;
+
+    Animation move, fadeIn;
+    ImageView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        textView=findViewById(R.id.signup);
-        textView.bringToFront();
+        setContentView(R.layout.signup);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
