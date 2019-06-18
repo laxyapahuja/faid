71ddaa0b363485270713288437189ff86b0d9644
Laxya Pahuja <laxya.pahuja8@gmail.com>
2019-06-18 23:06:57 +0530

    home

--- app/src/main/java/com/example/faid/HomeActivity.java
+++ app/src/main/java/com/example/faid/HomeActivity.java
@@ -1,7 +1,10 @@
 package com.example.faid;
 
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void profileName(View view) {
+        TextInputEditText loginet = findViewById(R.id.email);
+        String email = loginet.getText().toString();
+        TextView profilenametv = findViewById(R.id.profilename);
+        profilenametv.setText(email);
     }
 }
