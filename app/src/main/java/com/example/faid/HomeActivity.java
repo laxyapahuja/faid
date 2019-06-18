16ba9be9d63b572463799a5fd3bf74125e9c372b
Laxya Pahuja <laxya.pahuja8@gmail.com>
2019-06-19 03:02:23 +0530

    HomeActivity Basic UI + Image Cropper + Request Permissions + Pick from Gallery

--- app/src/main/java/com/example/faid/HomeActivity.java
+++ app/src/main/java/com/example/faid/HomeActivity.java
@@ -1,10 +1,19 @@
 package com.example.faid;
 
 
+import android.Manifest;
+import android.app.Activity;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputEditText;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import android.content.Intent;
@@ -14,35 +23,98 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
 
 
 public class HomeActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    private StorageReference mStorageRef;
+    private static int RESULT_LOAD_IMG = 1;
+    String imgDecodableString;
     View view;
-
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Intent intent  = getIntent();
+        checkPermissions();
+        Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
-        String data = bundle.getString("state","");
-       if (data.equals("signup")){
-          Snackbar.make(findViewById(android.R.id.content), "Sign Up Successful",Snackbar.LENGTH_SHORT).show();
-       }
-       else{
-           Snackbar.make(findViewById(android.R.id.content), "Log In Successful",Snackbar.LENGTH_SHORT).show();
-       }
+        String data = bundle.getString("state", "");
+        if (data.equals("signup")) {
+            Snackbar.make(findViewById(android.R.id.content), "Sign Up Successful", Snackbar.LENGTH_SHORT).show();
+        } else {
+            Snackbar.make(findViewById(android.R.id.content), "Log In Successful", Snackbar.LENGTH_SHORT).show();
+        }
         mAuth = FirebaseAuth.getInstance();
+        mStorageRef = FirebaseStorage.getInstance().getReference();
         String email = mAuth.getCurrentUser().getEmail().toString();
         TextView profilenametv = findViewById(R.id.profilename);
         profilenametv.setText(email);
     }
 
-    public void profileName(View view) {
+        private void checkPermissions() {
+            int permissionCheck = HomeActivity.this.checkSelfPermission("Manifest.permission.WRITE_EXTERNAL_STORAGE");
+            if (permissionCheck != 0) {
+                ActivityCompat.requestPermissions(HomeActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                       1001);
+            }
+        }
+
+        public void selectImage(View view) {
+            // Create intent to Open Image applications like Gallery, Google Photos
+            Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                    android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            // Start the Intent
+            startActivityForResult(galleryIntent, RESULT_LOAD_IMG);
+        }
+
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+            try {
+                // When an Image is picked
+                if (requestCode == RESULT_LOAD_IMG && resultCode == RESULT_OK
+                        && null != data) {
+                    // Get the Image from data
+
+                    Uri selectedImage = data.getData();
+                    String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+                    // Get the cursor
+                    Cursor cursor = getContentResolver().query(selectedImage,
+                            filePathColumn, null, null, null);
+                    // Move to first row
+                    cursor.moveToFirst();
+
+                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                    imgDecodableString = cursor.getString(columnIndex);
+                    cursor.close();
+                    ImageView imgView = (ImageView) findViewById(R.id.profilepic);
+                    // Set the Image in ImageView after decoding the String
+                    imgView.setImageBitmap(BitmapFactory
+                            .decodeFile(imgDecodableString));
+
+                } else {
+                    Toast.makeText(this, "You haven't picked Image",
+                            Toast.LENGTH_LONG).show();
+                }
+            } catch (Exception e) {
+                Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG)
+                        .show();
+            }
+
+        }
 
     }
-}
+
+
+
